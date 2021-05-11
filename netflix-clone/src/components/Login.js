9baa52cff7043284f8db4f82108abb3ed9d30a40
bhzoghi <bh.zoghi@hotmail.com>
2021-05-11 09:02:43 -0400

    login page is completed, next step is signup

--- netflix-clone/src/components/Login.js
+++ netflix-clone/src/components/Login.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/login.css";
 
 function Login() {
+  const [signIn, setSignIn] = useState(false);
   return (
     <div className="loginContainer">
       <img
         className="netflix__logo"
         src="https://pngimg.com/uploads/netflix/netflix_PNG11.png"
       />
-      <button className="loginButton">Sign In</button>
+      <button className="loginButton" onClick={() => setSignIn(true)}>
+        Sign In
+      </button>
       <div className="loginGradient" />
       <div className="container__background"></div>
+      <div className="login__body">
+        <h1>Unlimited films, TV programmes and more.</h1>
+        <h2>Whatch anywhere, cancel anytime!</h2>
+        <h3>
+          Ready to watch? Enter your email to create or restart your membership.
+        </h3>
+        <div className="login__input">
+          <form>
+            <input placeholder="Email Address..." type="email" />
+            <button className="login__getStarted">GET STARTED</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }

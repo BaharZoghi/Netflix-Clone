ace474676d33631141e588e53797fcc6193fc30a
bhzoghi <bh.zoghi@hotmail.com>
2021-06-05 07:01:20 -0400

    firebase authentication is added

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./components/HomeScreen";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { auth } from "./firebase";
 
 function App() {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //login
+      } else {
+        //logout
+      }
+    });
+  }, []);
   return (
     <div className="App">
       <Router>
--- netflix-clone/src/components/SignIn.js
+++ netflix-clone/src/components/SignIn.js
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../styles/signin.css";
+import { auth } from "../firebase";
 
 function SignIn() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const signInFunc = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
   return (
     <div className="signInContainer">
       <h1>Sign In</h1>
       <form>
-        <input placeholder="Email..." type="Email" />
-        <input placeholder="Password..." type="password" />
-        <button type="submit">Sign In</button>
+        <input ref={emailRef} placeholder="Email..." type="Email" />
+        <input ref={passwordRef} placeholder="Password..." type="password" />
+        <button type="submit" onClick={signInFunc}>
+          Sign In
+        </button>
         <h4>
           <span className="gray__text">New to Netflix? </span>
-          <span className="signup__button">Sign up now.</span>
+          <span className="signup__button" onClick={register}>
+            Sign up now.
+          </span>
         </h4>
       </form>
     </div>
